de42c2173c48347bc3fe49195eb1ed895549969b
priyanshuraj96 <157800513+priyanshuraj96@users.noreply.github.com>
2024-10-03 22:19:13 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: 1,
+    name: "mumbai",
+    price: 25000,
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.6d8UMEZ5VEfPUszkEcN_jQHaD4&pid=Api&P=0&h=180",
+    info: "Mumbai, known as the 'City of Dreams,' offers a vibrant blend of rich history, diverse cultures, and modern marvels. As India's financial capital, it's a bustling metropolis where colonial-era architecture stands beside soaring skyscrapers. On a tour, you can explore iconic landmarks like the Gateway of India, enjoy a peaceful walk along Marine Drive, or experience the vibrant street life at Colaba Causeway. Don't miss the chance to visit cultural spots like the Chhatrapati Shivaji Maharaj Terminus and Elephanta Caves, while indulging in local delicacies like vada pav and pav bhaji.",
+  },
+  {
+    id: 2,
+    name: "bihar",
+    price: 30000,
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.pFiua_TPWOvTNXBOdXGwpgHaD4&pid=Api&P=0&h=180",
+    info: "Bihar, a state steeped in history and culture, offers a fascinating tour experience for those interested in exploring India's ancient heritage. Known as the birthplace of Buddhism, it houses the sacred Mahabodhi Temple in Bodh Gaya, where Lord Buddha attained enlightenment. Tourists can also visit historical sites like Nalanda University, one of the world's oldest centers of learning, and the ruins of ancient Pataliputra. Bihar's rich cultural tapestry is complemented by its scenic beauty, with the Ganges River flowing through its plains, making it an intriguing destination for history buffs and spiritual seekers alike.",
+  },
+  {
+    id: 3,
+    name: "ooty",
+    price: 50000,
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.Kk-e7pPh5DVv38Y7tqvEfAHaD8&pid=Api&P=0&h=180",
+    info: "Ooty, often called the 'Queen of Hill Stations,' is a popular tourist destination nestled in the Nilgiri Hills of Tamil Nadu. Known for its cool climate, lush tea gardens, and scenic landscapes, Ooty offers a peaceful retreat from city life. Tourists can enjoy the breathtaking views from Doddabetta Peak, take a leisurely boat ride on Ooty Lake, and explore the beautiful Botanical Gardens. A ride on the famous Nilgiri Mountain Railway, a UNESCO World Heritage site, adds to the charm of visiting Ooty. The hill station is perfect for nature lovers, honeymooners, and adventure enthusiasts alike.",
+  },
+  {
+    id: 4,
+    name: "kashmir",
+    price: 30000,
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.6aKZJQQj5QomvJfV__yhpAHaEK&pid=Api&P=0&h=180",
+    info: "Kashmir, often referred to as 'Paradise on Earth,' is renowned for its stunning natural beauty, with snow-capped mountains, verdant valleys, and serene lakes. A tour to Kashmir offers breathtaking experiences like a shikara ride on the tranquil Dal Lake, a stay in traditional houseboats, and the mesmerizing beauty of the Mughal Gardens in Srinagar. Popular destinations like Gulmarg and Pahalgam are perfect for adventure seekers with opportunities for skiing, trekking, and golfing. In winter, Kashmir transforms into a winter wonderland, while in summer, the valleys bloom with colorful flowers, making it an idyllic year-round destination for tourists.",
+  },
+];
+
+export default data;
